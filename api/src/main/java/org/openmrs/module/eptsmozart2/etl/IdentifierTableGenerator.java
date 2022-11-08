bfd0cbaf242b97d2553a7bc4481af0a60a71a768
Willa Mhawila <a.mhawila@gmail.com>
2022-11-08 05:49:42 +0300

    MOZ2-92: Removed identifier_type_name from identifier table

--- api/src/main/java/org/openmrs/module/eptsmozart2/etl/IdentifierTableGenerator.java
+++ api/src/main/java/org/openmrs/module/eptsmozart2/etl/IdentifierTableGenerator.java
@@ -30,8 +30,8 @@ public class IdentifierTableGenerator extends AbstractGenerator {
 		if (batchSize == null)
 			batchSize = Integer.MAX_VALUE;
 		String insertSql = new StringBuilder("INSERT INTO ").append(Mozart2Properties.getInstance().getNewDatabaseName())
-		        .append(".identifier (patient_uuid, identifier_type, identifier_type_name, identifier_value, `primary`, ")
-		        .append("identifier_uuid) VALUES (?, ?, ?, ?, ?, ?)").toString();
+		        .append(".identifier (patient_uuid, identifier_type, identifier_value, `primary`, ")
+		        .append("identifier_uuid) VALUES (?, ?, ?, ?, ?)").toString();
 		try {
 			if (insertStatement == null) {
 				insertStatement = ConnectionPool.getConnection().prepareStatement(insertSql);
@@ -42,10 +42,9 @@ public class IdentifierTableGenerator extends AbstractGenerator {
 			while (results.next() && count < batchSize) {
 				insertStatement.setString(1, results.getString("patient_uuid"));
 				insertStatement.setInt(2, results.getInt("identifier_type"));
-				insertStatement.setString(3, results.getString("name"));
-				insertStatement.setString(4, results.getString("identifier"));
-				insertStatement.setBoolean(5, results.getBoolean("primary"));
-				insertStatement.setString(6, results.getString("identifier_uuid"));
+				insertStatement.setString(3, results.getString("identifier"));
+				insertStatement.setBoolean(4, results.getBoolean("primary"));
+				insertStatement.setString(5, results.getString("identifier_uuid"));
 				
 				insertStatement.addBatch();
 				++count;
@@ -72,30 +71,19 @@ public class IdentifierTableGenerator extends AbstractGenerator {
 	
 	@Override
 	protected String countQuery() {
-		return new StringBuilder("SELECT COUNT(*) FROM ")
-		        .append(Mozart2Properties.getInstance().getDatabaseName())
-		        .append(".patient_identifier pi JOIN ")
-		        .append(Mozart2Properties.getInstance().getNewDatabaseName())
-		        .append(".patient p ON pi.patient_id = p.patient_id JOIN ")
-		        .append(Mozart2Properties.getInstance().getDatabaseName())
-		        .append(
-		            ".patient_identifier_type pit on pi.identifier_type = pit.patient_identifier_type_id AND !pit.retired ")
-		        .append("WHERE !pi.voided").toString();
+		return new StringBuilder("SELECT COUNT(*) FROM ").append(Mozart2Properties.getInstance().getDatabaseName())
+		        .append(".patient_identifier pi JOIN ").append(Mozart2Properties.getInstance().getNewDatabaseName())
+		        .append(".patient p ON pi.patient_id = p.patient_id AND !pi.voided").toString();
 	}
 	
 	@Override
 	protected String fetchQuery(Integer start, Integer batchSize) {
-		StringBuilder sb = new StringBuilder(
-		        "SELECT pi.patient_id, p.patient_uuid, pi.identifier_type, pit.name, pi.identifier, ")
+		StringBuilder sb = new StringBuilder("SELECT pi.patient_id, p.patient_uuid, pi.identifier_type, pi.identifier, ")
 		        .append("pi.preferred as 'primary', pi.uuid as identifier_uuid FROM ")
-		        .append(Mozart2Properties.getInstance().getDatabaseName())
-		        .append(".patient_identifier pi JOIN ")
+		        .append(Mozart2Properties.getInstance().getDatabaseName()).append(".patient_identifier pi JOIN ")
 		        .append(Mozart2Properties.getInstance().getNewDatabaseName())
-		        .append(".patient p ON pi.patient_id = p.patient_id JOIN ")
-		        .append(Mozart2Properties.getInstance().getDatabaseName())
-		        .append(
-		            ".patient_identifier_type pit on pi.identifier_type = pit.patient_identifier_type_id AND !pit.retired ")
-		        .append("WHERE !pi.voided ORDER BY pi.patient_identifier_id");
+		        .append(".patient p ON pi.patient_id = p.patient_id AND !pi.voided ")
+		        .append("ORDER BY pi.patient_identifier_id");
 		
 		if (start != null) {
 			sb.append(" limit ?");
--- api/src/main/resources/identifier.sql
+++ api/src/main/resources/identifier.sql
@@ -2,7 +2,6 @@ CREATE TABLE `identifier` (
   `identifier_seq` int(11) NOT NULL AUTO_INCREMENT,
   `patient_uuid` char(38) DEFAULT NULL,
   `identifier_type` int(11) DEFAULT NULL,
-  `identifier_type_name` varchar(255) DEFAULT NULL,
   `identifier_value` varchar(255) DEFAULT NULL,
   `primary` tinyint(4) DEFAULT NULL,
   `identifier_uuid` varchar(38) DEFAULT NULL,
